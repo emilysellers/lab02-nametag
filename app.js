b4dad5dbf247c0b489ec0a381ca756c133ff7bd0
Emily Sellers <emilymelindasellers@gmail.com>
2021-06-02 17:58:19 -0700

    updating buttons

--- app.js
+++ app.js
@@ -5,20 +5,20 @@
 const submitBtn = document.getElementById('submit-btn');
 const inputField = document.getElementById('input-field');
 const middleSection = document.getElementById('middle-section');
-const colorField = document.getElementById('color-field')
+const colorField = document.getElementById('color-field');
 const topSection = document.getElementById('top-section');
 const btmSection = document.getElementById('bottom-section');
 const pronounBtn = document.getElementById('pronoun-btn');
 const pronounField = document.getElementById('pronoun-input');
 const nameSpan = document.getElementById('name');
-const pronounSpan =document.getElementById('pronoun');
+const pronounSpan = document.getElementById('pronoun');
 
 submitBtn.addEventListener('click', ()=>{
     topSection.style.backgroundColor = colorField.value;
     btmSection.style.backgroundColor = colorField.value;
     nameSpan.textContent = inputField.value;
-})
+});
 
 pronounBtn.addEventListener('click', ()=>{
     pronounSpan.textContent = pronounField.value;
-})
+});
